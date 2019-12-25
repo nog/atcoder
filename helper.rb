d6c0126ca591ec8e376c9663bf6b9fdb403a0f70
nog <nog@diverse-inc.com>
2019-12-25 19:21:36 +0900

    langをマップから取るように変更

--- helper.rb
+++ helper.rb
@@ -1,43 +1,59 @@
 #! /usr/bin/env ruby
 
-contest = ARGV[0]
-task    = ARGV[1]
-lang    = ARGV[2]
-command = ARGV[3]
+def main
+  contest = ARGV[0]
+  task    = ARGV[1]
+  lang    = get_lang(ARGV[2])
+  command = ARGV[3]
 
-contest_dir = "#{__dir__}/contests/#{contest}"
-task_dir = "#{contest_dir}/#{task}"
-task_url = "https://atcoder.jp/contests/#{contest}/tasks/#{contest}_#{task}"
+  contest_dir = "#{__dir__}/contests/#{contest}"
+  task_dir = "#{contest_dir}/#{task}"
+  task_url = "https://atcoder.jp/contests/#{contest}/tasks/#{contest}_#{task}"
 
-script_file = case lang
-when "ruby"
-  "#{task_dir}/main.rb"
-when "go"
-  "#{task_dir}/main.go"
-else
-  raise
-end
-
-case command
-when "setup"
-  `mkdir -p #{task_dir}`
-  `touch #{script_file}`
-  `cd #{task_dir};oj download #{task_url}`
-when "test"
-  test_command = case lang
-  when  "ruby"
-    "ruby #{script_file}"
-  when "go"
-    "go run #{script_file}"
+  script_file = case lang
+  when :ruby
+    "#{task_dir}/main.rb"
+  when :golang
+    "#{task_dir}/main.go"
   else
     raise
   end
-  
-  `cd #{task_dir};oj test -c "#{test_command}"`
-when "submit"
-  `cd #{task_dir};oj submit #{task_url} #{script_file} -y`
-when "git"
-  `git add #{task_dir};git commit -m "[#{lang}]#{contest}-#{task}";git push`
-else
-  raise
+
+
+  case command
+  when "setup"
+    `mkdir -p #{task_dir}`
+    `touch #{script_file}`
+    `cd #{task_dir};oj download #{task_url}`
+  when "test"
+    test_command = case lang
+    when  :ruby
+      "ruby #{script_file}"
+    when :golang
+      "go run #{script_file}"
+    else
+      raise
+    end
+
+    `cd #{task_dir};oj test -c "#{test_command}"`
+  when "submit"
+    `cd #{task_dir};oj submit #{task_url} #{script_file} -y`
+  when "git"
+    `git add #{task_dir};git commit -m "[#{lang}]#{contest}-#{task}";git push`
+  else
+    raise
+  end
 end
+
+def get_lang(name)
+  result = {
+    ruby: :ruby,
+    rb: :ruby,
+    go: :golang,
+    golang: :golang
+  }[name.to_sym]
+  raise unless result
+  result
+end
+
+main()
