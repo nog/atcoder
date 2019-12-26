b6c6a0c3d39116ff02fc900129468630672c1536
nog <nog@diverse-inc.com>
2019-12-27 01:21:27 +0900

    [golang]agc015-a

---
+++ contests/agc015/a/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n := nextInt()
+	a := nextInt()
+	b := nextInt()
+	if a == b {
+		fmt.Println(1)
+		return
+	}
+	if a > b {
+		fmt.Println(0)
+		return
+	}
+	if n <= 1 {
+		fmt.Println(0)
+		return
+	}
+	fmt.Println((n-2)*(b-a) + 1)
+}
+
+// 引数処理
+func next() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func nextInt() int {
+	a, _ := strconv.Atoi(next())
+	return a
+}
+
+func nextFloat64() float64 {
+	a, _ := strconv.ParseFloat(next(), 64)
+	return a
+}
+
+func nextInts(n int) []int {
+	ret := make([]int, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextInt()
+	}
+	return ret
+}
+
+func nextFloats(n int) []float64 {
+	ret := make([]float64, n)
+	for i := 0; i < n; i++ {
+		ret[i] = nextFloat64()
+	}
+	return ret
+}
+
+func nextStrings(n int) []string {
+	ret := make([]string, n)
+	for i := 0; i < n; i++ {
+		ret[i] = next()
+	}
+	return ret
+}
