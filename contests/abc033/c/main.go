56c1ad3571e3cbe771d56f2af7cdf72479e8f63a
nog <nog@diverse-inc.com>
2019-12-27 21:28:27 +0900

    [golang]abc033-c

---
+++ contests/abc033/c/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	io := newIo()
+	defer io.Flush()
+	s := io.Next() + "+"
+	result := 0
+	for i := 0; i < len(s); i += 2 {
+		x, _ := strconv.Atoi(string(s[i]))
+		// fmt.Println(x)
+		switch s[i+1] {
+		case '+':
+			if x != 0 {
+				result++
+			}
+		case '*':
+			hasZero := (x == 0)
+			for {
+				i += 2
+				x, _ := strconv.Atoi(string(s[i]))
+				if x == 0 {
+					hasZero = true
+				}
+				if i > len(s) {
+					if !hasZero {
+						result++
+					}
+					break
+				}
+				if s[i+1] == '+' {
+					if !hasZero {
+						result++
+					}
+					break
+				}
+			}
+		default:
+			fmt.Println(string(s[i+1]))
+			panic("error")
+		}
+	}
+	fmt.Println(result)
+}
+
+// 引数処理
+
+type io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func newIo() *io {
+	return &io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
