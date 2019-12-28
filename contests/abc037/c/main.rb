9cc75e4bd8ca3216810c36d6eeb44e91965b54d3
nog <nog@diverse-inc.com>
2019-12-28 14:18:30 +0900

    [ruby]abc037-c

---
+++ contests/abc037/c/main.rb
@@ -0,0 +1,12 @@
+n, k = gets.chomp.split(' ').map(&:to_i)
+list = gets.chomp.split(' ').map(&:to_i)
+sum = 0
+sum_list = list.map do |x|
+  sum += x
+end
+
+result = 0
+(n - k +1).times do |i|
+  result += sum_list[i+k -1] - (i == 0 ? 0 : sum_list[i-1])
+end
+puts result
