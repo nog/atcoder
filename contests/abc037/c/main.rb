74c1c8a3fcc5e00e8ee52f5c8ac575e998dfd6f1
nog <nog@diverse-inc.com>
2019-12-28 14:27:46 +0900

    [ruby]abc037-c

--- contests/abc037/c/main.rb
+++ contests/abc037/c/main.rb
@@ -6,7 +6,10 @@ sum_list = list.map do |x|
 end
 
 result = 0
-(n - k +1).times do |i|
-  result += sum_list[i+k -1] - (i == 0 ? 0 : sum_list[i-1])
+# (n - k +1).times do |i|
+#   result += sum_list[i+k -1] - (i == 0 ? 0 : sum_list[i-1])
+# end
+k.times do |i|
+  result += sum_list[-i-1] - ( i == 0 ? 0 : sum_list[i-1])
 end
 puts result
