69a4a93d2d221f3c326e065ce0c5c2a2fd8a2155
nog <nog@diverse-inc.com>
2019-12-30 17:01:23 +0900

    [ruby]agc041-b

---
+++ contests/agc041/b/main.rb
@@ -0,0 +1,32 @@
+N,M,V,P = gets.split(' ').map(&:to_i)
+A = gets.split(' ').map(&:to_i).sort
+
+require 'pp'
+PP.pp [N,M,V,P], STDERR
+PP.pp A, STDERR
+def check a
+  return true if a >= A[-P]
+  return false if A[-P] > a + M
+  if V <= P
+    return A[-P] <= a + M
+  end
+
+  point = (V-P) * M
+  skip_self = false
+  A[0,A.size - P].each do |aa|
+    if aa == a && !skip_self
+      skip_self = true
+      next
+    end
+    if aa <= a
+      point -= M
+    elsif aa > a && (a+M) > aa
+      point -= (a+M-aa)
+    end
+    return true if point <= 0
+  end
+  return true if point <= 0
+  return point <= 0
+end
+line = A.bsearch{|a| check a }
+puts A.find_all{|a| a >= line}.size
