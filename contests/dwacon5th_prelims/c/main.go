714447294322d5d54685e771f966d5b2a30c1347
nog <nog@diverse-inc.com>
2020-01-02 12:41:16 +0900

    [golang]dwacon5th_prelims-c

---
+++ contests/dwacon5th_prelims/c/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	io := newIo()
+	defer io.Flush()
+	N := io.NextInt()
+	S := io.Next()
+	Q := io.NextInt()
+	K := make([]int, Q)
+	for q := range K {
+		K[q] = io.NextInt()
+	}
+
+	var dList []int
+	sumM := make([]int, N+1)
+	sumC := make([]int, N+1)
+	sumMC := make([]int, N+1)
+	sumM[0] = 0
+	sumC[0] = 0
+	sumMC[0] = 0
+
+	for i, s := range S {
+		sumC[i+1] = sumC[i]
+		sumMC[i+1] = sumMC[i]
+
+		if s == 'D' {
+			dList = append(dList, i)
+		}
+		if s == 'M' {
+			sumM[i+1] = sumM[i] + 1
+		} else {
+			sumM[i+1] = sumM[i]
+		}
+		if s == 'C' {
+			sumC[i+1] = sumC[i] + 1
+			sumMC[i+1] = sumMC[i] + sumM[i]
+		} else {
+			sumC[i+1] = sumC[i]
+			sumMC[i+1] = sumMC[i]
+		}
+	}
+	for _, k := range K {
+		result := 0
+		for _, d := range dList {
+			to := d + k
+			if to > N {
+				to = N
+			}
+			result += (sumMC[to] - sumMC[d]) - ((sumC[to] - sumC[d]) * sumM[d])
+		}
+		fmt.Println(result)
+	}
+}
+
+type io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func newIo() *io {
+	return &io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
