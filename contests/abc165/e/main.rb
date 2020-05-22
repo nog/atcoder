8bf5ee62ed814efdbb67a19cbb11da0727cef2ba
nog <11278+nog@users.noreply.github.com>
2020-05-22 23:02:55 +0900

    精進

--- contests/abc165/e/main.rb
+++ contests/abc165/e/main.rb
@@ -4,6 +4,11 @@ n, m = gets.split.map(&:to_i)
 # x
 # x + 開催回数 - 1
 # x + 開催回数 - 1 - n
+# x - y
+# y - x + n
+# 1 10000 10000
+# 1 10000
+# 1 + 9999 , 10000 + 9999 - 10000
 m.times do |i|
   puts [i+1, n-i-1].join(' ')
 end
