673ce480cd07c382e12ecc676c1a595e933baf54
nog <11278+nog@users.noreply.github.com>
2020-06-04 12:35:03 +0900

    精進

--- contests/abc166/e/main.rb
+++ contests/abc166/e/main.rb
@@ -1,54 +1,27 @@
 n = gets.to_i
 a = gets.split.map(&:to_i)
 
-sums = []
+# (i - j).abs =  a[i] + a[j]
+# i - a[i] = j + a[j]
+# or
+# - i - a[i] = - j + a[j]
+# i + a[i] = j - a[j]
+
+
+diff = {}
+sum = {}
 result = 0
-list = (0..(n-1)).to_a
-
-sorted_a = a.sort
-# sorted_list = list.sort_by do |i|
-#   a[i]
-# end
-
-minsum = sorted_a[0] + sorted_a[1]
-
-(n-1).downto(0) do |i|
-  if a[i] >= n
-    list.delete_at(i)
+n.times do |i|
+  d = (i+1) - a[i]
+  s = (i+1) + a[i]
+  if sum[d]
+    result += sum[d]
   end
+  diff[d] ||= 0
+  diff[d] += 1
+  sum[s] ||= 0
+  sum[s] += 1
+
 end
 
-# list.combination(2) do |b,c|
-#   diff = (b-c).abs
-#   if diff == (a[b] + a[c])
-#     result += 1
-#   end
-# end
-
-# while m = sorted_list.shift do
-#   sorted_list.each do |t|
-#     diff = t - m
-#     sum = a[m] + a[t]
-#     if sum == diff
-#       result += 1
-#     end
-#     if sum >= n
-#       break
-#     end
-#   end
-# end
-
-while m = list.shift do
-  list.reverse.each do |t|
-    diff = t - m
-    sum = a[m] + a[t]
-    if sum == diff
-      result += 1
-    end
-    if diff <= minsum
-      break
-    end
-  end
-end
-
-puts result 
+puts result
